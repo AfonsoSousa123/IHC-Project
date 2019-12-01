504e557f21766b67b6aea4d2096c8a0f9e890504
AfonsoSousa123 <afonsosousa10@gmail.com>
2019-12-01 13:13:24 +0000

    Update 2.2

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<header id="header" id="home">     
+  <div class="container main-menu">
+      <div class="row align-items-center justify-content-between d-flex">
+        <div id="logo" class="col-lg-7">
+          <a href="/">
+              <img src="https://www.uma.pt/wp-content/themes/uma/images/Logo_UMa_site-01.png" width="150" height="50" style="float:right;" alt="UMA Logo"/>
+          </a>
+        </div>
+
+        <nav id="nav-menu-container" >
+          <ul class="nav-menu">
+            <li class="menu-has-children"><a href="">Menu</a>
+              <ul>
+                  <li><a href="">Sobre</a></li>		
+                  <li><a href="">Admissão</a></li>		
+                  <li><a href="">Ensino</a></li>
+                  <li><a href="">Investigação</a></li>				                		
+              </ul>
+            </li>					          					          		          
+          </ul>
+        </nav><!-- #nav-menu-container -->		    		
+      </div>
+  </div>
+</header><!-- #header -->
